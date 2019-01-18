98b722e8641be65229db410566b0380438dff74b
mogyi5 <szaboki.reka@gmail.com>
2019-01-18 21:02:36 +0000

    After a successful chapter 5
    Uploaded changes after chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -31,12 +31,12 @@ def populate():
 	"url":"http://flask.pocoo.org"}
 	]
 	
-	cats = {"Python": {"pages":python_pages, views = 128, likes = 64},
-			"Django": {"pages":django_pages, views = 64, likes = 32},
-			"Other FrameWorks": {"pages":other_pages, views = 32, likes = 16}}
+	cats = {"Python": {"pages":python_pages, 'views' : 128, 'likes' : 64},
+			"Django": {"pages":django_pages, 'views' : 64, 'likes' :32},
+			"Other Frameworks": {"pages":other_pages, 'views': 32, 'likes' : 16},}
 			
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(cat, cat_data["views"], cat_data["likes"])
 		for p in cat_data["pages"]:
 			add_page(c, p["title"], p["url"])
 			
@@ -51,10 +51,10 @@ def add_page(cat, title, url, views=0):
 	p.save()
 	return p
 	
-def add_cat(name, views, likes):
+def add_cat(name, views=0, likes=0):
 	c = Category.objects.get_or_create(name=name)[0]
 	c.views = views
-	c.likes=likes
+	c.likes = likes
 	c.save()
 	return c
 
