29bd37ffc90498e3f5a9af3824c1c10e04731549
mogyi5 <szaboki.reka@gmail.com>
2019-01-18 20:35:09 +0000

    Another commit - chapter 5

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	context_dict = {'boldmessage': "This tutorial has been put together by Reka."}
+	context_dict = {'boldmessage': 'This tutorial has been put together by Reka.'}
 	return render(request, 'rango/about.html', context=context_dict)
