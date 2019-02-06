058022ac69d785f5316ec6a4f0f12042effec7d7
mogyi5 <szaboki.reka@gmail.com>
2019-02-06 22:41:06 +0000

    ..found the error......

--- rango/views.py
+++ rango/views.py
@@ -12,8 +12,7 @@ def index(request):
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	context_dict = {'boldmessage': 'This tutorial has been put together by Reka.'}
-	return render(request, 'rango/about.html', context=context_dict)
+	return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
 	context_dict = {}
