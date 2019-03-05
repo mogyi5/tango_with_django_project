0705b8ac01d71af30e1d81547777b10afa47fcb0
mogyi5 <szaboki.reka@gmail.com>
2019-03-05 12:43:16 +0000

    after chap 12

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+#from rango.models import Recipe, Ingredient, Review
 from rango.models import UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,6 +11,14 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
-admin.site.register(UserProfile)
+
+#----------------------------------------------------
+# this is ours below, we dont need to look at ingredients or reviews individually right
+# just looking at the recipes and users
+# class RecipeAdmin(admin.ModelAdmin):
+# 	prepopulated_fields = {'slug':('title',)}
+#
+# admin.site.register(Recipe, RecipeAdmin)
+# admin.site.register(UserProfile)
 
 # Register your models here.
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -130,4 +131,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/rango/login/'
+LOGIN_URL = '/accounts/login/'
+REGISTRATION_OPEN = True
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+REGISTRATION_AUTO_LOGIN = True
+
+LOGIN_REDIRECT_URL = '/rango/'
